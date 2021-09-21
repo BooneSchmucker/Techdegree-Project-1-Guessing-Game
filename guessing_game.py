85f9836ff9ecd512c2db717a4cc6b27a1cb48835
BooneSchmucker <booneschmucker1987@gmail.com>
2021-09-21 15:18:24 -0600

    updated number parameters

--- guessing_game.py
+++ guessing_game.py
@@ -12,7 +12,7 @@ prompt1 = "Guess the magic number between 1 and 10!  "
 lower = "      ***It's lower. Try again***  "
 higher = "      ***It's higher. Try again***  "
 end = "Play again to see if you can beat your high score?"
-NIR = "***GuessNumber3000 only uses numbers 1 - 10***"
+NIR = "***GuessNumber3000 only uses numbers 1 - 10.***"
 compute = "Hmm.. DOES NOT COMPUTE.. Try 1 - 10 again.  "
 yay = "Hurray! GuessNumber3000 is happy! :)"
 sad = "GuessNumber3000 is sad to see you go. :("
@@ -21,16 +21,16 @@ tie = "***You tied your high score!***"
 
 print (welcome_message)
 
-high_score = 10                                      #every high score is new high score (FIX IT)
-
 def start_game():
     magic_number = random.randint(1,10)
     guess_count = 1
+    high_score = 10
     while True:
         try:
             guess = int(input(prompt1))
             if guess < 1 or guess > 10:
                 print (NIR)
+                continue
         except ValueError:
             print (compute)
         else:
@@ -42,9 +42,9 @@ def start_game():
                 print (lower)
             elif guess == magic_number:
                 break
-    print ("Got It! It took you {} guesses.".format(guess_count))
+    print ("Got It! It took you {} guesses.".format(guess_count)) #insert here
     if guess_count < high_score:
-        highscore = int("{}".format(guess_count))
+        high_score = int("{}".format(guess_count))
         print (HS)
     elif guess_count == high_score:
         print (tie)
@@ -52,7 +52,7 @@ def start_game():
     play_again = str(input("press Y to play again.  "))
     if play_again.upper() == "Y":
         print (yay)
-        print ("Enter the GuessNumber3000 hall of fame by beating {} guesses.".format(highscore))
+        print ("Enter the GuessNumber3000 hall of fame by beating {} guesses.".format(high_score))
         start_game()
     else:
         sys.exit(sad)
