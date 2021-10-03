29c3a81d2eeadcd79f19e0ab18f0263a25262409
BooneSchmucker <booneschmucker1987@gmail.com>
2021-10-03 15:17:39 -0600

    Update guessing_game.py
    Fixed high score

--- guessing_game.py
+++ guessing_game.py
@@ -1,13 +1,7 @@
-"""
-Python Web Development Techdegree
-Project 1 - Number Guessing Game
---------------------------------
-"""
-
 import random
 import sys
 
-welcome_message = "Welcome to GuessNumber3000!"
+welcome_message = ("Welcome to GuessNumber3000! I am thinking of a number between 1 and 10. Can you guess the number?")
 prompt1 = "Guess the magic number between 1 and 10!  "
 lower = "      ***It's lower. Try again***  "
 higher = "      ***It's higher. Try again***  "
@@ -17,15 +11,15 @@ compute = "Hmm.. DOES NOT COMPUTE.. Try 1 - 10 again.  "
 yay = "Hurray! GuessNumber3000 is happy! :)"
 sad = "GuessNumber3000 is sad to see you go. :("
 HS = "***New High Score! Welcome to the record books!***"
-tie = "***You tied your high score!***"
 
 print (welcome_message)
 
 def start_game():
     magic_number = random.randint(1,10)
     guess_count = 1
-    high_score = 10
     while True:
+        scores = [10]
+        high_score = min(scores)
         try:
             guess = int(input(prompt1))
             if guess < 1 or guess > 10:
@@ -42,18 +36,17 @@ def start_game():
                 print (lower)
             elif guess == magic_number:
                 break
-    print ("Got It! It took you {} guesses.".format(guess_count)) #insert here
-    if guess_count < high_score:
-        high_score = int("{}".format(guess_count))
+    print ("Got It! It took you {} guesses.".format(guess_count))
+    scores.append(guess_count)
+    if guess_count == min(scores):
         print (HS)
-    elif guess_count == high_score:
-        print (tie)
     print (end)
     play_again = str(input("press Y to play again.  "))
     if play_again.upper() == "Y":
         print (yay)
-        print ("Enter the GuessNumber3000 hall of fame by beating {} guesses.".format(high_score))
+        print ("Enter the GuessNumber3000 hall of fame by beating {} guesses.".format(min(scores)))
         start_game()
     else:
         sys.exit(sad)
 start_game()
+            
